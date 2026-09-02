b23bf21d918df6b62c1a96f2eab420529b565d5f
ymchoi <yumeen04@naver.com>
2026-09-02 17:55:55 +0900

    upload

--- 00-대전_2반/09.02/swea-1221-GNS.py
+++ 00-대전_2반/09.02/swea-1221-GNS.py
@@ -1,18 +1,19 @@
-dictionary = {"ZRO": 0, "ONE": 1, "TWO": 2, "THR": 3, "FOR": 4, "FIV": 5, "SIX": 6, "SVN": 7, "EGT": 8, "NIN": 9}
-
+#dictionary = {"ZRO": 0, "ONE": 1, "TWO": 2, "THR": 3, "FOR": 4, "FIV": 5, "SIX": 6, "SVN": 7, "EGT": 8, "NIN": 9}
+numbers = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
 T = int(input())
 for tc in range(1, T+1):
     num, N = input().split()
     words = list(input().split())
     words_index = []
+    sorted_words = []
 
-    for word in range(int(N)):
-        words_index.append(dictionary[word])
+    for word in words:
+        words_index.append(numbers.index(word))
 
     words_index.sort()
 
     for idx in words_index:
-        sorted_words = [k for k, v in dictionary.items() if v == idx]
+        sorted_words.append(numbers[idx])
 
     print(num)
-    print(sorted_words)
+    print(*sorted_words)
--- 00-대전_2반/09.02/swea-4466-최대성적표만들기.py
+++ 00-대전_2반/09.02/swea-4466-최대성적표만들기.py
@@ -2,4 +2,4 @@ T = int(input())
 for tc in range(1, T+1):
     N, K = map(int, input().split())
     scores = sorted(list(map(int, input().split())), reverse=True) 
-    print(f'#{tc} {sum(scores[:K + 1])}')
+    print(f'#{tc} {sum(scores[:K])}')
